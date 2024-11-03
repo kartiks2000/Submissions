b68c274528c8121e6a4a8cda1af1269cdacc41c5
Kartik saxena <kartiksaxena@kartiks-mbp.home>
2024-11-03 00:30:56 -0400

    Add: incomplete routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import {SubmissionsComponent} from './src/app/components/submissions/submissions.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: SubmissionsComponent
+    },
+    {
+        path: 'forms',
+        redirectTo: 'www.google.com'
+    },
+    {
+        path: 'Customers',
+        redirectTo: 'www.google.com'
+    },
+    {
+        path: 'submissions',
+        component: SubmissionsComponent
+    },
+    {
+        path: 'history',
+        redirectTo: 'www.google.com'
+    },
+    {
+        path: 'reports',
+        redirectTo: 'www.google.com'
+    },
+    {
+        path: 'workflows',
+        redirectTo: 'www.google.com'
+    },
+];
