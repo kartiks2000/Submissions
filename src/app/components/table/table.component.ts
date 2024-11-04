9f0e27dc845ab5ff97afc409b6c14f9818de2418
Kartik saxena <kartiksaxena@kartiks-mbp.home>
2024-11-04 11:34:49 -0500

    Feat: Added pagination

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,16 +1,37 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 import { Task } from '../../interfaces/task.interface';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgbPaginationModule],
   templateUrl: './table.component.html',
-  styleUrl: './table.component.css',
+  styleUrls: ['./table.component.css'],
 })
-export class TableComponent {
+export class TableComponent implements OnInit, OnChanges {
   @Input() tasks: Task[] = [];
+  paginatedTasks: Task[] = [];
+  page = 1;
+  pageSize = 10;
+
+  ngOnInit() {
+    this.updatePaginatedTasks();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['tasks']) {
+      this.updatePaginatedTasks();
+    }
+  }
+
+  onPageChange() {
+    this.updatePaginatedTasks();
+  }
+
+  updatePaginatedTasks() {
+    const startIndex = (this.page - 1) * this.pageSize;
+    this.paginatedTasks = this.tasks.slice(startIndex, startIndex + this.pageSize);
+  }
 }
